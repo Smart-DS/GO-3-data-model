3fcfe42233cc5aefb88122abb2ca043c5b248bbb
Tarek Elgindy <tarek.elgindy@nrel.gov>
2022-03-22 01:39:48 -0600

    Test script (with correct output) to see if data file validates against schema

---
+++ datamodel/test.py
@@ -0,0 +1,15 @@
+#import Bid-DS-data-model.datamodel.input.data.InputDataFile
+import datamodel.base
+import datamodel.input.static
+import datamodel.input.timeseries
+import datamodel.input.data as data
+from datamodel.base import *
+import os
+
+path = '../input/json_data/'
+json_file = os.path.join(path,'PSY_RTS_GMLC_data.json')
+json_file = os.path.join(path,'PSY_RTS_GMLC_data_fixed_load_commit.json')
+json_file = os.path.join(path,'PSY_RTS_GMLC_data_fixed_load_commit_v2.json')
+test = data.InputDataFile.load(json_file)
+print('Succeeded!')
+
