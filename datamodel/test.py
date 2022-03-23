4aa4a0e5e0215027bac8edcb133b21fb247b7fcd
Tarek Elgindy <tarek.elgindy@nrel.gov>
2022-03-23 13:38:09 -0600

    new data files that conform to schema

--- datamodel/test.py
+++ datamodel/test.py
@@ -9,7 +9,7 @@ import os
 path = '../input/json_data/'
 json_file = os.path.join(path,'PSY_RTS_GMLC_data.json')
 json_file = os.path.join(path,'PSY_RTS_GMLC_data_fixed_load_commit.json')
-json_file = os.path.join(path,'PSY_RTS_GMLC_data_fixed_load_commit_v2.json')
+json_file = os.path.join(path,'PSY_RTS_GMLC_data_fixed_load_commit_v3_output.json')
 test = data.InputDataFile.load(json_file)
 print('Succeeded!')
 
