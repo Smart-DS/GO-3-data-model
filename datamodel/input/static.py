3e2ca6382bed830a30aed51a9f2c4c17a82bbc3a
Jesse T Holzer <jesse.holzer@pnnl.gov>
2022-06-02 14:03:41 -0700

    fixed the validator message

--- datamodel/input/static.py
+++ datamodel/input/static.py
@@ -9,9 +9,9 @@ class General(GeneralBase):
     def stop_is_after_start(cls, data):
         start = data.get("timestamp_start")
         stop = data.get("timestamp_stop")
-        if (start is not None) and (stop is not None) and (stop <= start):
+        if (start is not None) and (stop is not None) and not (start < stop):
             msg = "fails {} < {}. {}: {}, {}: {}".format(
-                "timestamp_stop", "timestamp_start", "timestamp_stop", stop, "timestamp_start", start)
+                "timestamp_start", "timestamp_stop", "timestamp_start", start, "timestamp_stop", stop)
             raise ValueError(msg)
         return data
 
