de6f767b6d7be65549bce46ed1d93629976e26e3
Jesse T Holzer <jesse.holzer@pnnl.gov>
2022-06-02 13:57:40 -0700

    add validators using base/derived class idea

--- datamodel/input/static.py
+++ datamodel/input/static.py
@@ -1,8 +1,21 @@
 import logging
+from pydantic import root_validator
 
 from datamodel.input.staticbase import *
 
-class General(GeneralBase): pass
+class General(GeneralBase):
+
+    @root_validator
+    def stop_is_after_start(cls, data):
+        start = data.get("timestamp_start")
+        stop = data.get("timestamp_stop")
+        if (start is not None) and (stop is not None) and (stop <= start):
+            msg = "fails {} < {}. {}: {}, {}: {}".format(
+                "timestamp_stop", "timestamp_start", "timestamp_stop", stop, "timestamp_start", start)
+            raise ValueError(msg)
+        return data
+
+    pass
 
 class ViolationCostsParameters(ViolationCostsParametersBase): pass
 
