748d2af5f59386bc89dbd6578a0143bf2b8a09e7
Jesse T Holzer <jesse.holzer@pnnl.gov>
2022-07-11 16:57:16 -0700

    add some uid checks

--- datamodel/input/reliability.py
+++ datamodel/input/reliability.py
@@ -1,5 +1,14 @@
 import logging
+from pydantic import root_validator, validator
 from datamodel.input.reliabilitybase import *
 
-class Contingency(ContingencyBase): pass
+class Contingency(ContingencyBase):
 
+    @validator("components")
+    def num_components_eq_q(cls, data):
+
+        if not (len(data) == 1):
+            msg = "fails {} == 1. {}: {}".format(
+                "len(components)", "len(components)", len(data))
+            raise ValueError(msg)
+        return data            
--- datamodel/input/sections.py
+++ datamodel/input/sections.py
@@ -5,11 +5,60 @@ class Network(NetworkBase):
 
     # todo bus UIDs unique
     # etc
-    pass
+
+    def get_bus_uids(self):
+
+        return [i.uid for i in self.bus]
+
+    def get_shunt_uids(self):
+
+        return [i.uid for i in self.shunt]
+
+    def get_simple_dispatchable_device_uids(self):
+
+        return [i.uid for i in self.simple_dispatchable_device]
+
+    def get_ac_line_uids(self):
+
+        return [i.uid for i in self.ac_line]
+
+    def get_two_winding_transformer_uids(self):
+
+        return [i.uid for i in self.two_winding_transformer]
+
+    def get_dc_line_uids(self):
+
+        return [i.uid for i in self.dc_line]
+
+    def get_active_zonal_reserve_uids(self):
+
+        return [i.uid for i in self.active_zonal_reserve]
+
+    def get_reactive_zonal_reserve_uids(self):
+
+        return [i.uid for i in self.reactive_zonal_reserve]
+
+    def get_uids(self):
+
+        uids = (
+            self.get_bus_uids() +
+            self.get_shunt_uids() +
+            self.get_simple_dispatchable_device_uids() +
+            self.get_ac_line_uids() +
+            self.get_two_winding_transformer_uids() +
+            self.get_dc_line_uids() +
+            self.get_active_zonal_reserve_uids() +
+            self.get_reactive_zonal_reserve_uids())
+        return uids
 
 class TimeSeriesInput(TimeSeriesInputBase): pass
 
 class Reliability(ReliabilityBase):
 
-    # todo ctg UIDs unique?
-    pass
+    def get_contingency_uids(self):
+
+        return [i.uid for i in self.contingency]
+
+    def get_uids(self):
+
+        return self.get_contingency_uids()
