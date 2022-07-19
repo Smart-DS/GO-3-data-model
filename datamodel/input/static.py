08f31b4876e31ff75a27d2bab51bb5e9637e586f
Jesse T Holzer <jesse.holzer@pnnl.gov>
2022-07-19 09:08:30 -0700

    update

--- datamodel/input/static.py
+++ datamodel/input/static.py
@@ -123,6 +123,17 @@ class Shunt(ShuntBase):
 
 class DispatchableDevices_SimpleProducingConsumingDevices(DispatchableDevices_SimpleProducingConsumingDevicesBase):
 
+    @validator("startup_states")
+    def startup_states_entry0_ge_0(cls, data):
+
+        num_states = len(data)
+        idx_err = [i for i in range(num_states) if data[i][0] < 0.0]
+        errs = [(i, data[i][0]) for i in idx_err]
+        if len(idx_err) > 0:
+            msg = "fails states have entry 0 >= 0. failures (index, entry 0): {}".format(errs)
+            raise ValueError(msg)
+        return data
+
     @validator("startups_ub")
     def startups_ub_entry0_ge_0(cls, data):
 
@@ -334,26 +345,28 @@ class DispatchableDevices_SimpleProducingConsumingDevices(DispatchableDevices_Si
             raise ValueError(msg)
         return data
 
+    @root_validator
+    def q_linear_or_bound_cap_but_not_both(cls, data):
 
-
-
-
-
-
-        # "q_linear_cap": 0,
-        # "q_bound_cap": 1,
-        # "q_0_ub": 5.0,
-        # "q_0_lb": -5.0,
-        # "beta_ub": 2.0,
-        # "beta_lb": -0.5
-
-
-
-
-
-
-
+        linear = data.get("q_linear_cap")
+        bound = data.get("q_bound_cap")
+        if (linear is not None) and (bound is not None) and (linear > 0) and (bound > 0):
+            msg = "fails q_linear_cap == 0 or q_bound_cap == 0. q_linear_cap: {}, q_bound_cap: {}".format(
+                linear, bound)
+            raise ValueError(msg)
+        return data
+    
 class ACTransmissionLine(ACTransmissionLineBase):
+    
+    @root_validator
+    def fbus_ne_tbus(cls, data):
+
+        fr = data.get("fr_bus")
+        to = data.get("to_bus")
+        if (fr is not None) and (to is not None) and (fr == to):
+            msg = "fails fr_bus != to_bus. fr_bus: {}, to_bus: {}".format(fr, to)
+            raise ValueError(msg)
+        return data
 
     @validator("mva_ub_nom")
     def mva_ub_nom_gt_0(cls, data):
@@ -394,11 +407,259 @@ class ACTransmissionLine(ACTransmissionLineBase):
             raise ValueError(msg)
         return data
 
-class TwoWindingTransformer(TwoWindingTransformerBase): pass
+class TwoWindingTransformer(TwoWindingTransformerBase):
+    
+    @root_validator
+    def fbus_ne_tbus(cls, data):
 
-class DCLine(DCLineBase): pass
+        fr = data.get("fr_bus")
+        to = data.get("to_bus")
+        if (fr is not None) and (to is not None) and (fr == to):
+            msg = "fails fr_bus != to_bus. fr_bus: {}, to_bus: {}".format(fr, to)
+            raise ValueError(msg)
+        return data
 
-class ActiveZonalReserveRequirementsViolationCosts(ActiveZonalReserveRequirementsViolationCostsBase): pass
+    @validator("mva_ub_nom")
+    def mva_ub_nom_gt_0(cls, data):
 
-class ReactiveZonalReserveRequirementsViolationCosts(ReactiveZonalReserveRequirementsViolationCostsBase): pass
+        if not (data > 0):
+            msg = "fails {} > 0. {}: {}".format(
+                "mva_ub_nom", "mva_ub_nom", data)
+            raise ValueError(msg)
+        return data            
+
+    @validator("mva_ub_em")
+    def mva_ub_em_gt_0(cls, data):
+
+        if not (data > 0):
+            msg = "fails {} > 0. {}: {}".format(
+                "mva_ub_em", "mva_ub_em", data)
+            raise ValueError(msg)
+        return data            
+    
+    @root_validator
+    def mva_ub_nom_le_em(cls, data):
+
+        nom = data.get("mva_ub_nom")
+        em = data.get("mva_ub_em")
+        if (nom is not None) and (em is not None) and not (nom <= em):
+            msg = "fails {} <= {}. {}: {}, {}: {}".format(
+                "mva_ub_nom", "mva_ub_em", "mva_ub_nom", nom, "mva_ub_em", em)
+            raise ValueError(msg)
+        return data
+    
+    @root_validator
+    def r_ne_0_or_x_ne_0(cls, data):
+
+        r = data.get("r")
+        x = data.get("x")
+        if (r is not None) and (x is not None) and not (abs(r) + abs(x) > 0):
+            msg = "fails abs(r) + abs(x) > 0. r: {}, x: {}".format(r, x)
+            raise ValueError(msg)
+        return data
+    
+    @root_validator
+    def tm_le_tm_ub(cls, data):
+
+        tm_ub = data.get("tm_ub")
+        initial_status = data.get("initial_status")
+        if (tm_ub is not None) and (initial_status is not None):
+            tm = initial_status.tm
+            if not (tm <= tm_ub):
+                msg = "fails initial_status.tm <= tm_ub. initial_status.tm: {}, tm_ub: {}".format(tm, tm_ub)
+                raise ValueError(msg)
+        return data
+    
+    @root_validator
+    def tm_ge_tm_lb(cls, data):
+
+        tm_lb = data.get("tm_lb")
+        initial_status = data.get("initial_status")
+        if (tm_lb is not None) and (initial_status is not None):
+            tm = initial_status.tm
+            if not (tm >= tm_lb):
+                msg = "fails initial_status.tm >= tm_lb. initial_status.tm: {}, tm_lb: {}".format(tm, tm_lb)
+                raise ValueError(msg)
+        return data
+    
+    @root_validator
+    def ta_le_ta_ub(cls, data):
+
+        ta_ub = data.get("ta_ub")
+        initial_status = data.get("initial_status")
+        if (ta_ub is not None) and (initial_status is not None):
+            ta = initial_status.ta
+            if not (ta <= ta_ub):
+                msg = "fails initial_status.ta <= ta_ub. initial_status.ta: {}, ta_ub: {}".format(ta, ta_ub)
+                raise ValueError(msg)
+        return data
+    
+    @root_validator
+    def ta_ge_ta_lb(cls, data):
+
+        ta_lb = data.get("ta_lb")
+        initial_status = data.get("initial_status")
+        if (ta_lb is not None) and (initial_status is not None):
+            ta = initial_status.ta
+            if not (ta >= ta_lb):
+                msg = "fails initial_status.ta >= ta_lb. initial_status.ta: {}, ta_lb: {}".format(ta, ta_lb)
+                raise ValueError(msg)
+        return data
+
+    @root_validator
+    def tm_or_ta_lb_eq_ub(cls, data):
+        
+        tm_ub = data.get("tm_ub")
+        tm_lb = data.get("tm_lb")
+        ta_ub = data.get("ta_ub")
+        ta_lb = data.get("ta_lb")
+        if (tm_ub is not None) and (tm_lb is not None) and (ta_ub is not None) and (ta_lb is not None):
+            if (tm_lb < tm_ub) and (ta_lb < ta_ub):
+                msg = "fails tm_ub == tm_lb or ta_ub == ta_lb. tm_ub: {}, tm_lb: {}, ta_ub: {}, ta_lb: {}".format(
+                    tm_ub, tm_lb, ta_ub, ta_lb)
+                raise ValueError(msg)
+        return data
+
+    @validator("tm_lb")
+    def tm_lb_gt_0(cls, data):
+
+        if not (data > 0):
+            msg = "fails {} > 0. {}: {}".format(
+                "tm_lb", "tm_lb", data)
+            raise ValueError(msg)
+        return data            
+
+class DCLine(DCLineBase):
+    
+    @root_validator
+    def fbus_ne_tbus(cls, data):
+
+        fr = data.get("fr_bus")
+        to = data.get("to_bus")
+        if (fr is not None) and (to is not None) and (fr == to):
+            msg = "fails fr_bus != to_bus. fr_bus: {}, to_bus: {}".format(fr, to)
+            raise ValueError(msg)
+        return data
+    
+    @root_validator
+    def pdc_fr_le_pdc_ub(cls, data):
+
+        pdc_ub = data.get("pdc_ub")
+        initial_status = data.get("initial_status")
+        if (pdc_ub is not None) and (initial_status is not None):
+            pdc_fr = initial_status.pdc_fr
+            if not (pdc_fr <= pdc_ub):
+                msg = "fails initial_status.pdc_fr <= pdc_ub. initial_status.pdc_fr: {}, pdc_ub: {}".format(
+                    pdc_fr, pdc_ub)
+                raise ValueError(msg)
+        return data
+    
+    @root_validator
+    def pdc_fr_ge_minus_pdc_ub(cls, data):
+
+        pdc_ub = data.get("pdc_ub")
+        initial_status = data.get("initial_status")
+        if (pdc_ub is not None) and (initial_status is not None):
+            pdc_fr = initial_status.pdc_fr
+            if not (pdc_fr >= -pdc_ub):
+                msg = "fails initial_status.pdc_fr >= -pdc_ub. initial_status.pdc_fr: {}, pdc_ub: {}".format(
+                    pdc_fr, pdc_ub)
+                raise ValueError(msg)
+        return data
+    
+    @root_validator
+    def qdc_fr_le_qdc_fr_ub(cls, data):
+
+        qdc_fr_ub = data.get("qdc_fr_ub")
+        initial_status = data.get("initial_status")
+        if (qdc_fr_ub is not None) and (initial_status is not None):
+            qdc_fr = initial_status.qdc_fr
+            if not (qdc_fr <= qdc_fr_ub):
+                msg = "fails initial_status.qdc_fr <= qdc_fr_ub. initial_status.qdc_fr: {}, qdc_fr_ub: {}".format(
+                    qdc_fr, qdc_fr_ub)
+                raise ValueError(msg)
+        return data
+    
+    @root_validator
+    def qdc_fr_ge_qdc_fr_lb(cls, data):
+
+        qdc_fr_lb = data.get("qdc_fr_lb")
+        initial_status = data.get("initial_status")
+        if (qdc_fr_lb is not None) and (initial_status is not None):
+            qdc_fr = initial_status.qdc_fr
+            if not (qdc_fr >= qdc_fr_lb):
+                msg = "fails initial_status.qdc_fr >= qdc_fr_lb. initial_status.qdc_fr: {}, qdc_fr_lb: {}".format(
+                    qdc_fr, qdc_fr_lb)
+                raise ValueError(msg)
+        return data
+    
+    @root_validator
+    def qdc_to_le_qdc_to_ub(cls, data):
+
+        qdc_to_ub = data.get("qdc_to_ub")
+        initial_status = data.get("initial_status")
+        if (qdc_to_ub is not None) and (initial_status is not None):
+            qdc_to = initial_status.qdc_to
+            if not (qdc_to <= qdc_to_ub):
+                msg = "fails initial_status.qdc_to <= qdc_to_ub. initial_status.qdc_to: {}, qdc_to_ub: {}".format(
+                    qdc_to, qdc_to_ub)
+                raise ValueError(msg)
+        return data
+    
+    @root_validator
+    def qdc_to_ge_qdc_to_lb(cls, data):
+
+        qdc_to_lb = data.get("qdc_to_lb")
+        initial_status = data.get("initial_status")
+        if (qdc_to_lb is not None) and (initial_status is not None):
+            qdc_to = initial_status.qdc_to
+            if not (qdc_to >= qdc_to_lb):
+                msg = "fails initial_status.qdc_to >= qdc_to_lb. initial_status.qdc_to: {}, qdc_to_lb: {}".format(
+                    qdc_to, qdc_to_lb)
+                raise ValueError(msg)
+        return data
+
+    @validator("qdc_fr_ub")
+    def qdc_fr_ub_ge_0(cls, data):
+
+        if not (data >= 0):
+            msg = "fails {} >= 0. {}: {}".format(
+                "qdc_fr_ub", "qdc_fr_ub", data)
+            raise ValueError(msg)
+        return data            
+
+    @validator("qdc_fr_lb")
+    def qdc_fr_lb_le_0(cls, data):
+
+        if not (data <= 0):
+            msg = "fails {} <= 0. {}: {}".format(
+                "qdc_fr_lb", "qdc_fr_lb", data)
+            raise ValueError(msg)
+        return data            
+
+    @validator("qdc_to_ub")
+    def qdc_to_ub_ge_0(cls, data):
+
+        if not (data >= 0):
+            msg = "fails {} >= 0. {}: {}".format(
+                "qdc_to_ub", "qdc_to_ub", data)
+            raise ValueError(msg)
+        return data            
+
+    @validator("qdc_to_lb")
+    def qdc_to_lb_le_0(cls, data):
+
+        if not (data <= 0):
+            msg = "fails {} <= 0. {}: {}".format(
+                "qdc_to_lb", "qdc_to_lb", data)
+            raise ValueError(msg)
+        return data            
+
+class ActiveZonalReserveRequirementsViolationCosts(ActiveZonalReserveRequirementsViolationCostsBase):
+
+    pass
+
+class ReactiveZonalReserveRequirementsViolationCosts(ReactiveZonalReserveRequirementsViolationCostsBase):
+
+    pass
 
--- datamodel/input/timeseries.py
+++ datamodel/input/timeseries.py
@@ -1,11 +1,42 @@
 import logging
+from pydantic import root_validator, validator
+
 from datamodel.input.timeseriesbase import *
 
-class General(GeneralBase): pass
+class General(GeneralBase):
 
-class DispatchableDevices_SimpleProducingConsumingDevices(DispatchableDevices_SimpleProducingConsumingDevicesBase): pass
+    @root_validator
+    def time_periods_eq_len_interval_duration(cls, data):
 
-class ActiveZonalReserveRequirementsViolationCosts(ActiveZonalReserveRequirementsViolationCostsBase): pass
+        time_periods = data.get("time_periods")
+        interval_duration = data.get("interval_duration")
+        if (time_periods is not None) and (interval_duration is not None):
+            if not(len(interval_duration) == time_periods):
+                msg = "fails len(interval_duration) == time_periods. len(interval_duration): {}, time_periods: {}".format(
+                    len(interval_duration), time_periods)
+                raise ValueError(msg)
+        return data
 
-class ReactiveZonalReserveRequirementsViolationCosts(ReactiveZonalReserveRequirementsViolationCostsBase): pass
+    @validator("interval_duration")
+    def interval_duration_gt_0(cls, data):
+
+        num_t = len(data)
+        idx_err = [i for i in range(num_t) if data[i] <= 0.0]
+        errs = [(i, data[i]) for i in idx_err]
+        if len(idx_err) > 0:
+            msg = "fails entries > 0. failures (index, entry): {}".format(errs)
+            raise ValueError(msg)
+        return data
+
+class DispatchableDevices_SimpleProducingConsumingDevices(DispatchableDevices_SimpleProducingConsumingDevicesBase):
+    
+    pass
+
+class ActiveZonalReserveRequirementsViolationCosts(ActiveZonalReserveRequirementsViolationCostsBase):
+
+    pass
+
+class ReactiveZonalReserveRequirementsViolationCosts(ReactiveZonalReserveRequirementsViolationCostsBase):
+
+    pass
 
